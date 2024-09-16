23ada6c004dc9c090f0796393ae6fed109668c7d
Hager <125750506+HagerMustafaa@users.noreply.github.com>
2024-09-16 14:46:48 +0300

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  String name;
+  name = 'Ninja';
+  //memory []=> name
+
+  print('my name is hager');
+  print('hager enjoys reading books');
+  print('hager enjoys painting');
+  print('my name is $name');
+  print('$name enjoys reading books');
+  print('$name enjoys painting');
+}
